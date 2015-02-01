f5c6924ba97649dd4547e57dc9a425b9ae90f832
Gordan Sajević <gsajevic@hotmail.com>
2015-02-01 18:18:53 +0100

    initial commit

---
+++ src/Crawling.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Crawling {
+	
+	private String file;
+	private String url; 
+	private final String path = System.getProperty("user.home") + File.separator + "workspace" + 
+			File.separator + "ThreadingHomework" + File.separator + "src" + File.separator + 
+			"Files" + File.separator + file;
+	public static int counter = 0;
+	
+	public Crawling(String file, String url) 
+	{
+		this.file = file;
+		this.url = url;
+		crawlingUrl();
+	}
+	
+	private void crawlingUrl()
+	{
+		
+		try {
+			URL address = new URL(this.url);
+			URLConnection connection = address.openConnection();
+			InputStream in = connection.getInputStream();
+			FileOutputStream fos = new FileOutputStream(path);
+			URL currentUrl = connection.getURL();
+			int bytes = 0;
+			byte[] buffer = new byte[1024];
+			while ((bytes = in.read(buffer)) >= 0) {
+				fos.write(buffer, 0, bytes);
+
+			}
+			PrintWriter out = new PrintWriter(new FileOutputStream(file, true));
+			out.print(currentUrl.toString());
+			fos.close();
+			in.close();
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/Threads.java
@@ -0,0 +1,19 @@
+
+public class Threads extends Thread{
+	
+	private String fileName;
+	private String url;
+	
+	public Threads(String fileName, String url) 
+	{
+		this.fileName = fileName;
+		this.url = url;
+	}
+	
+	public void run()
+	{
+		Crawling crawler = new Crawling(fileName, url);
+		crawler.counter++;
+	}
+
+}
